c253d6e2a1f20aadebd84a13487a1c45676c2948
Alexey Slivka <slivka77@inbox.ru>
2016-03-07 18:27:53 +0300

    Добавлены pundit и rolify.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306135142) do
+ActiveRecord::Schema.define(version: 20160307152055) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -50,6 +50,17 @@ ActiveRecord::Schema.define(version: 20160306135142) do
   end
 
   add_index "organizations", ["name"], name: "index_organizations_on_name"
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], name: "index_roles_on_name"
 
   create_table "scanned_ports", force: :cascade do |t|
     t.datetime "job_time"
@@ -134,5 +145,12 @@ ActiveRecord::Schema.define(version: 20160306135142) do
   add_index "users", ["email"], name: "index_users_on_email"
   add_index "users", ["name"], name: "index_users_on_name"
   add_index "users", ["organization_id"], name: "index_users_on_organization_id"
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
 
 end
