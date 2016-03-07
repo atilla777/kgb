566a7997e2b2c10cf80e07064d40e1ebfff7fb63
Alexey Slivka <slivka77@inbox.ru>
2016-03-07 19:37:52 +0300

    Включена аутентификация при входе в систему.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user_session, :current_user
+  before_filter :authenticate?
 
   private
+    def authenticate?
+      unless current_user
+        redirect_to :sign_in
+      end
+    end
 
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,4 +1,5 @@
 class UserSessionsController < ApplicationController
+   skip_before_filter :authenticate?
 
   def new
     @user_session = UserSession.new
