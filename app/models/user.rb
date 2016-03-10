5db0794152e763365659563ca1a0091f757aa017
atilla777 <slivka77@inbox.ru>
2016-03-10 13:31:21 +0300

    Доработана проверка пароля и почты при сохранении пользователя.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,17 @@ class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.crypto_provider = Authlogic::CryptoProviders::Sha512 # алгоритм хэширования пароля
     c.merge_validates_format_of_email_field_options message: 'должно быть адресом электронной почты'
-    c.merge_validates_length_of_password_field_options({:minimum => 6}) # минимальная длина пароля
-    # не проверять пароль и электронную почту для пользователя которому не разрешен вход (поле active для которого не установлено)
-    c.merge_validates_confirmation_of_password_field_options if: :active
-    c.merge_validates_length_of_password_confirmation_field_options if: :active
-    # !!! сделать проверку уникальности этого поля (password), если это поле не пустое
-    c.merge_validates_length_of_password_field_options if: :active
-    c.merge_validates_format_of_email_field_options if: :active
-    # !!! сделать проверку уникальности этого поля (email), если это поле не пустое
     c.merge_validates_uniqueness_of_email_field_options if: :active
+    c.merge_validates_length_of_password_field_options({:minimum => 6}) # минимальная длина пароля
+    # не проверять пароль и электронную почту для пользователя которому не разрешен вход (поле active для которого не установлено) или если пароль не вводится
+    c.merge_validates_confirmation_of_password_field_options :if => Proc.new {
+      (self.password.present? or self.password_confirmation.present?) or (self.new_record? and self.active)}
+    c.merge_validates_length_of_password_confirmation_field_options :if => Proc.new {
+      (self.password.present? or self.password_confirmation.present?) or (self.new_record? and self.active)}
+    c.merge_validates_length_of_password_field_options :if => Proc.new {
+      (self.password.present? or self.password_confirmation.present?) or (self.new_record? and self.active)}  c.merge_validates_length_of_password_field_options if: :active
+    c.merge_validates_format_of_email_field_options if: :active
+
   end
 
   before_save :set_activity
