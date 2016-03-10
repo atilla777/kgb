1d436b027c6b59a81d825f3d2745356a6d73d8a3
atilla777 <slivka77@inbox.ru>
2016-03-10 10:26:52 +0300

    Проверка авторизации для управления ролями.

--- app/controllers/users_roles_controller.rb
+++ app/controllers/users_roles_controller.rb
@@ -1,23 +1,29 @@
 class UsersRolesController < ApplicationController
 
+  before_filter :set_user
+
   def create
+    authorize :users_roles
     @user.add_role(params[:role])
     set_user_roles
     respond_to do |format|
-      # возращается ответ на ajax запрос (запрос со страницы сведений о работе)
+      # РІРѕР·СЂР°С‰Р°РµС‚СЃСЏ РѕС‚РІРµС‚ РЅР° ajax Р·Р°РїСЂРѕСЃ (Р·Р°РїСЂРѕСЃ СЃРѕ СЃС‚СЂР°РЅРёС†С‹ СЃРІРµРґРµРЅРёР№ Рѕ СЂР°Р±РѕС‚Рµ)
       format.js {render 'user_roles_renew'}
     end
   end
 
   def destroy
+    authorize :users_roles
     @user.remove_role(params[:role])
     set_user_roles
     respond_to do |format|
-      # возращается ответ на ajax запрос (запрос со страницы сведений о работе)
+      # РІРѕР·СЂР°С‰Р°РµС‚СЃСЏ РѕС‚РІРµС‚ РЅР° ajax Р·Р°РїСЂРѕСЃ (Р·Р°РїСЂРѕСЃ СЃРѕ СЃС‚СЂР°РЅРёС†С‹ СЃРІРµРґРµРЅРёР№ Рѕ СЂР°Р±РѕС‚Рµ)
       format.js {render 'user_roles_renew'}
     end
   end
-  
+
+  private
+
   def set_user
     @user = User.find(params[:user_id])
   end
@@ -26,6 +32,6 @@ class UsersRolesController < ApplicationController
     @roles = @user.roles
     user_roles_names = @roles.map{|role| role.name.to_sym}
     @allowed_roles = User.roles.keys.select{|role_name| user_roles_names.exclude?(role_name) }
-  en
-  
+  end
+
 end
