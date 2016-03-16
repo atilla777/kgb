3246ee5d779a638a7046b9fd1230a72518c29af4
atilla777 <slivka77@inbox.ru>
2016-03-16 16:06:59 +0300

    Update organization.rb

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -13,5 +13,16 @@ class Organization < ActiveRecord::Base
   def self.beholder_role_name
     :beholder
   end
+  
+   def current_services
+    #organizations_ids = policy_scope(Organization).pluck(:id)
+    organizations_ids = OrganizationPolicy::Scope.
+      new(UserSession.find.user, Organization).
+      resolve.
+      pluck(:id)
+
+    ScannedPort.where("scanned_ports.organization_id IN (#{organizations_ids.join(', ')})").
+      group(:organiuzation_id, :host_ip, :number, :state, :protocol)
+  end
 
 end
