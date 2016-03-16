24fe030d1e38c6c4c6b765341fddc741234875f6
Alexey Slivka <slivka77@inbox.ru>
2016-03-16 22:15:37 +0300

    Доработана панель управления.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,8 @@ class Organization < ActiveRecord::Base
 
   resourcify
 
+  #scope :ip_addresses, ->{services.select(:host_ip).distinct.pluck(:host_ip)}
+
   validates :name, length: {minimum: 3, maximum: 255}
   validates :name, uniqueness: true
 
@@ -13,16 +15,23 @@ class Organization < ActiveRecord::Base
   def self.beholder_role_name
     :beholder
   end
-  
-   def current_services
-    #organizations_ids = policy_scope(Organization).pluck(:id)
-    organizations_ids = OrganizationPolicy::Scope.
-      new(UserSession.find.user, Organization).
-      resolve.
-      pluck(:id)
 
-    ScannedPort.where("scanned_ports.organization_id IN (#{organizations_ids.join(', ')})").
-      group(:organiuzation_id, :host_ip, :number, :state, :protocol)
+  def ip_addresses
+    self.services.select(:host_ip).distinct.pluck(:host_ip)
+  end
+
+  def detected_services
+    #organization_ips = self.services.select(:host_ip).distinct.pluck(:host_ip)
+    ScannedPort.where(host_ip: self.ip_addresses).group(:number)#"host_ip IN (#{self.ip_addresses.join(', ')})")
+    #group(:host_ip)
+    #organizations_ids = policy_scope(Organization).pluck(:id)
+    #organizations_ids = OrganizationPolicy::Scope.
+    #  new(UserSession.find.user, Organization).
+    #  resolve.
+    #  pluck(:id)
+    #
+    #ScannedPort.where("scanned_ports.organization_id IN (#{organizations_ids.join(', ')})").
+    #  group(:organiuzation_id, :host_ip, :number, :state, :protocol)
   end
 
 end
