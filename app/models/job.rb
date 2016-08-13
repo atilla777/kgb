56a6cfaff64bb89752df9b065dadfc8915348ff7
Alexey <slivka77@inbox.ru>
2016-08-13 20:19:03 +0300

    Новая форма диапазона адресов хостов.

--- app/models/job.rb
+++ app/models/job.rb
@@ -8,11 +8,15 @@ class Job < ActiveRecord::Base
   validates :name, length: {minimum: 3, maximum: 255}
   validates :name, uniqueness: {scope: :organization_id}
   validates :options, length: {minimum: 3, maximum: 255}, allow_blank: true
-  validate :ports, :ports_format
+  # to do
+  #validate :ports, :ports_format
   validates :hosts, presence: true
-  validate :hosts, :hosts_format
+  # to do
+  #validate :hosts_format
   validates :organization_id, numericality: {only_integer: true}
   validates :option_set_id, numericality: {only_integer: true}
+
+  before_save :range1_to_range2
 
   scope :today_jobs, ->{
     joins(:schedules)
@@ -20,11 +24,13 @@ class Job < ActiveRecord::Base
                             week_day: Time.now.wday, month_day: Time.now.day))}
 
   private
-  def ports_format
-
-  end
-
-  def hosts_format
-
+  # make range like 192.168.1-16 from range like 192.168.1-192.168.16
+  def range1_to_range2
+    range = /\A(?<start_ip_d1_3>#{Service.ip4_d1_3_regexp})(?<start_ip_d4>#{Service.ip4_d4_regexp})-(?<end_ip_d1_3>#{Service.ip4_d1_3_regexp})(?<end_ip_d4>#{Service.ip4_d4_regexp})\z/.match(self.hosts)
+    if range
+      if range[:start_ip_d1_3] == range[:end_ip_d1_3]
+        self.hosts = "#{range[:start_ip_d1_3]}#{range[:start_ip_d4]}-#{range[:end_ip_d4]}"
+      end
+    end
   end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,8 @@ class Service < ActiveRecord::Base
   include Datatableable
 
   IP4_REGEXP = /(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])/
+  IP4_D1_3_REGEXP = /(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}/
+  IP4_D4_REGEXP = /([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])/
 
   PROTOCOLS = ['tcp', 'udp']
 
@@ -26,6 +28,13 @@ class Service < ActiveRecord::Base
     IP4_REGEXP
   end
 
+  def self.ip4_d4_regexp
+    IP4_D4_REGEXP
+  end
+
+  def self.ip4_d1_3_regexp
+    IP4_D1_3_REGEXP
+  end
 
   def self.legalities
     LEGALITIES
@@ -81,8 +90,8 @@ class Service < ActiveRecord::Base
 
   # Make array of ip addresses from array of ip adddresses and ip ranges
   # Example:
-  #   Service.normilized_hosts(['192.168.1.1', '192.168.1.2-192.168.1.3']) ->
-  #   ['192.168.1.1', '192.168.1.2', '192.168.1.3']
+  #   Service.normilized_hosts(['192.168.1.1', '192.168.1.2-192.168.1.3', '172.16.1-2']) ->
+  #   ['192.168.1.1', '192.168.1.2', '192.168.1.3', '172.16.1.1', '172.16.1.2']
   def self.normilize_hosts(hosts)
     normilized_hosts = []
     hosts.each do |host|
@@ -91,20 +100,29 @@ class Service < ActiveRecord::Base
         normilized_hosts += (IPAddr.new(range[:start_ip])..IPAddr.new(range[:end_ip])).map(&:to_s).to_a
       elsif /\A(#{ip4_regexp})\z/ =~ host
         normilized_hosts << host
+      elsif /\A(#{Service.ip4_d1_3_regexp})(#{Service.ip4_d4_regexp})-(#{Service.ip4_d4_regexp})\z/ =~ host
+        range2 = /\A(?<start_ip_d1_3>#{Service.ip4_d1_3_regexp})(?<start_ip_d4>#{Service.ip4_d4_regexp})-(?<end_ip_d4>#{Service.ip4_d4_regexp})\z/.match(host)
+        normilized_hosts += (IPAddr.new(range2[:start_ip_d1_3] << range2[:start_ip_d4])..IPAddr.new(range2[:start_ip_d1_3] << range2[:end_ip_d4])).map(&:to_s).to_a
       end
     end
     normilized_hosts.uniq
   end
 
   private
+
   def host_is_ip4_or_range
     range = /\A(?<start_ip>#{Service.ip4_regexp})-(?<end_ip>#{Service.ip4_regexp})\z/.match(host)
-    unless /\A#{Service.ip4_regexp}\z/ =~ host || range
+    range2 = /\A(?<start_ip_d1_3>#{Service.ip4_d1_3_regexp})(?<start_ip_d4>#{Service.ip4_d4_regexp})-(?<end_ip_d4>#{Service.ip4_d4_regexp})\z/.match(host)
+    unless /\A#{Service.ip4_regexp}\z/ =~ host || range || range2
       errors[:host] << I18n.t('errors.messages.must_be_ip4_or_range')
     end
     # check the second ip in range is greater then the first ip
     if range
-      unless IPAddr.new(range[:start_ip]).to_i < IPAddr.new(range[:end_ip]).to_i
+      if IPAddr.new(range[:start_ip]).to_i > IPAddr.new(range[:end_ip]).to_i
+        errors[:host] << I18n.t('errors.messages.must_be_ip4_or_range')
+      end
+    elsif range2
+      if range2[:start_ip_d4].to_i > range2[:end_ip_d4].to_i
         errors[:host] << I18n.t('errors.messages.must_be_ip4_or_range')
       end
     end
