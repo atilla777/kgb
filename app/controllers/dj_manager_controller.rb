3c2cbe2abd7d853be901d5c3b4bb81bf9dd1e393
Aleksey Slivka <slivka77@inbox.ru>
2017-04-27 21:14:05 +0300

    Organization editor can delete tasks

--- app/controllers/dj_manager_controller.rb
+++ app/controllers/dj_manager_controller.rb
@@ -1,18 +1,34 @@
 class DjManagerController < ApplicationController
   def index
     authorize :dj_manager
-    @tasks = Delayed::Job.all
+    #@tasks = Delayed::Job.all
+    #@tasks = policy_scope(Delayed::Job)#.includes(:jobs)
+    @tasks = DjManagerPolicy::Scope.new(current_user, Delayed::Job).resolve
   end
 
   def show
     authorize :dj_manager
-    @task = Delayed::Job.where(id: params[:id]).first
+    if current_user.has_role? :admin
+      @task = Delayed::Job.where(id: params[:id]).first
+    else
+      tasks = DjManagerPolicy::Scope.new(current_user, Delayed::Job).resolve
+      if tasks.include? Delayed::Job.where(id: params[:id]).first
+        @task = Delayed::Job.where(id: params[:id]).first
+      end
+    end
     redirect_to dj_index_path unless @task
   end
 
   def destroy
     authorize :dj_manager
-    task = Delayed::Job.where(id: params[:id]).where('locked_by IS NULL').first
+    if current_user.has_role? :admin
+      task = Delayed::Job.where(id: params[:id]).where('locked_by IS NULL').first
+    else
+      tasks = DjManagerPolicy::Scope.new(current_user, Delayed::Job).resolve
+      if tasks.include? Delayed::Job.where(id: params[:id]).first
+        task = Delayed::Job.where(id: params[:id]).where('locked_by IS NULL').first
+      end
+    end
     task.destroy if task
     redirect_to dj_index_path
   end
--- app/policies/dj_manager_policy.rb
+++ app/policies/dj_manager_policy.rb
@@ -1,6 +1,32 @@
 class DjManagerPolicy < ApplicationPolicy
+
+  class Scope < Scope
+    def resolve
+      if @user.has_any_role? :admin, :editor, :viewer
+        Delayed::Job.all
+      else
+        organizations_ids = OrganizationPolicy::Scope.
+          new(@user, Organization).resolve.pluck(:id)
+        jobs_ids = Job.where("organization_id IN (#{organizations_ids.join(', ')})").pluck(:id)
+        result = []
+        Delayed::Job.all.each do |delayed_job|
+          job_data = YAML.load(delayed_job.handler).job_data
+          if job_data['job_class'] == 'ScanJob'
+            s = job_data['arguments'][0]['_aj_globalid']
+            job_id = /\d+$/.match(s)[0].to_i
+            if jobs_ids.include? job_id
+              result << delayed_job
+            end
+          end
+        end
+        result
+      end
+    end
+  end
+
   def index?
-    if @user.has_role? :admin
+    #if @user.has_role? :admin
+    if @user.has_any_role? :admin, :editor, :viewer, :organization_editor, :organization_viewer
       true
     else
       false
@@ -8,7 +34,7 @@ class DjManagerPolicy < ApplicationPolicy
   end
 
   def show?
-    if @user.has_role? :admin
+    if @user.has_any_role? :admin, :editor, :viewer, :organization_editor, :organization_viewer
       true
     else
       false
@@ -16,7 +42,7 @@ class DjManagerPolicy < ApplicationPolicy
   end
 
   def destroy?
-    if @user.has_role? :admin
+    if @user.has_any_role? :admin, :organization_editor
       true
     else
       false
@@ -24,7 +50,7 @@ class DjManagerPolicy < ApplicationPolicy
   end
 
   def destroy_all?
-    if @user.has_role? :admin
+    if @user.has_any_role? :admin
       true
     else
       false
