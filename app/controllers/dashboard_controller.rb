7621a64bea1251ca94ac15f4fc162b53fe131e91
atilla777 <slivka77@inbox.ru>
2017-04-28 13:21:34 +0300

    Add files via upload

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,7 @@ class DashboardController < ApplicationController
 
   def detected_services
     authorize :dashboard
-    @user_active_services = current_user.jobs_active_services
+    #@user_active_services = current_user.jobs_active_services
   end
 
   def datatable
@@ -32,6 +32,7 @@ class DashboardController < ApplicationController
     fields += [{field: 'jobs.name', as: 'job_name', joins: 'jobs', on: 'jobs.id = scanned_ports.job_id'},
               {field: 'scanned_ports.host', as: 'sp_host'},
               {field: 'scanned_ports.port', as: 'sp_port', map_by_sql: "'<' || scanned_ports.port || '>'"},
+              {field: 'scanned_ports.port', as: 'port_id'},
               {field: 'scanned_ports.protocol', as: 'sp_protocol'},
               {field: 'scanned_ports.state', as: 'port_state', map_to: ScannedPort.states},
               {field: 'scanned_ports.state', as: 'state_id', invisible: true, filter: "scanned_ports.state = 'open'"},
@@ -69,7 +70,46 @@ map_by_sql:  "CASE
 
   def new_services
     authorize :dashboard
-    @user_active_services = current_user.jobs_active_services
+    #@user_active_services = current_user.jobs_active_services
+  end
+
+  def datatable2
+    authorize :dashboard
+    allowed_jobs_ids = current_user.jobs.pluck(:id)
+
+    fields = [{field: 'scanned_ports.job_time', as: 'job_time'},
+              {field: 'scanned_ports.id', as: 'id', invisible: true, sql_joins: %q(
+                                                                         INNER JOIN (SELECT scanned_ports.job_id,
+                                                                         MAX(scanned_ports.job_time)
+                                                                         AS 'max_time' FROM scanned_ports
+                                                                         GROUP BY scanned_ports.job_id)a
+                                                                         ON a.job_id = scanned_ports.job_id
+                                                                         AND a.max_time = scanned_ports.job_time
+                                                                        )},
+              {field: 'organizations.id', as: 'organization_id', invisible: true,},
+              {field: 'organizations.name', as: 'organization_name', joins: 'organizations', on: 'organizations.id = scanned_ports.organization_id'}]
+    fields << if current_user.has_any_role? :admin, :editor, :viewer
+                {field: 'scanned_ports.job_id', as: 'job_id', invisible: true}
+              else
+                {field: 'scanned_ports.job_id', as: 'job_id', invisible: true, filter: "scanned_ports.job_id IN (#{allowed_jobs_ids.join(',')})"}
+              end
+    fields += [{field: 'jobs.name', as: 'job_name', joins: 'jobs', on: 'jobs.id = scanned_ports.job_id'},
+              {field: 'scanned_ports.host', as: 'sp_host'},
+              {field: 'scanned_ports.port', as: 'sp_port', map_by_sql: "'<' || scanned_ports.port || '>'"},
+              {field: 'scanned_ports.port', as: 'port_id'},
+              {field: 'scanned_ports.protocol', as: 'sp_protocol'},
+              {field: 'scanned_ports.state', as: 'port_state', map_to: ScannedPort.states},
+              {field: 'scanned_ports.state', as: 'state_id', invisible: true, filter: "scanned_ports.state = 'open'"},
+              {field: 'services.id', as: 'service_id', invisible: true,
+               joins: 'services',
+               on: 'scanned_ports.port = services.port AND scanned_ports.host = services.host AND scanned_ports.protocol = services.protocol',
+                filter: 'services.id IS NULL'},
+              {field: 'scanned_ports.service', as: 'service'},
+              {field: "scanned_ports.product || ' ' || (CASE WHEN scanned_ports.product_version IS NULL THEN '' ELSE scanned_ports.product_version END) || ' ' || (CASE WHEN scanned_ports.product_extrainfo IS NULL THEN '' ELSE scanned_ports.product_extrainfo END)", as: 'product'}]
+    @datatable = ScannedPort.dt_all(params, fields)
+    respond_to do |format|
+      format.json {render 'datatable2'}
+    end
   end
 
   def hosts
