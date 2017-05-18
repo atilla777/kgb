21bf15a4d16bd84a729dd54c4977f9e0ad436691
atilla777 <slivka77@inbox.ru>
2017-05-18 15:36:00 +0300

    Update job_spec.rb

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Job, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before(:all) do
+    require 'database_cleaner'
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  context "create new" do
+    it "valid when host is www.hrundel.ru" do
+      expect(FactoryGirl.build(:job, hosts: 'www.hrundel.ru')).to have(0).errors.on(:hosts)
+    end
+    it "invalid when host is www./hrundel.ru" do
+      expect(FactoryGirl.build(:job, hosts: 'www.hrundel.ru')).to have(1).errors.on(:hosts)
+    end
+  end
 end
